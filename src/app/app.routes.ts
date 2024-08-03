c054d5dd0461a1a79bc52e3f444635137e71b742
Aditya <aditya149202514@gmail.com>
2024-08-04 02:04:25 +0530

    expense tracker angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'expense-tracker',loadChildren:()=> import('./expense-tracker/expense-tracker.module').then(m=> m.ExpenseTrackerModule)}
+];
